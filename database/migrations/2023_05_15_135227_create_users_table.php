68dfd3fbaeff7ba82498bc25c19fb0f2f9abc228
Afif Nur Fauzi <alscheift@gmail.com>
2023-05-15 21:21:12 +0700

    users table and User model

---
+++ database/migrations/2023_05_15_135227_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->string('username');
+            $table->string('password');
+            $table->boolean('is_admin')->default(false);
+            $table->string('nama');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
