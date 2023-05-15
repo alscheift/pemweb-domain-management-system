9ed1bdec7335ec9fa5027c5bc2bd98a6e7b28f66
dafinanz <112502897+dafinanz@users.noreply.github.com>
2023-05-15 20:25:54 +0700

    Add files via upload

---
+++ database/migrations/2023_05_15_123754_create_solutions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->id('id_penyelesaian');
+            $table->date('tanggal_penyelesaian');
+            $table->text('target_penyelesaian');
+            $table->string('status', 100);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('solutions');
+    }
+};
