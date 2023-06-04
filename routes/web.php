d516faccca793644e4f252a2dab07037085c05b0
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-04 14:58:02 +0700

    Login Page and Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('login', function () {
+    return view('sessions.create');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
