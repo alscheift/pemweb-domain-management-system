f6e5d0e8bb0684c5c7e7d325b6b04deb8f56b471
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-04 14:51:25 +0700

    Tailwind dark mode by class only

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html
-    lang="en-US" class="h-full bg-gray-100">
+    lang="en-US" class="h-full">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
     </title>
     @vite('resources/css/app.css')
 </head>
-<body class="h-full">
+<body class="h-full dark:bg-gray-900">
 
 <div class="min-h-full">
     {{$slot}}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
