dd6e5dad7625a8b605db5e80fb954bd1cdea708d
Afif Nur Fauzi <58578965+alscheift@users.noreply.github.com>
2023-06-13 12:06:18 +0700

    ...? (#6)
    Fix branch not being synched

--- resources/views/dashboard/admin/domains/index.blade.php
+++ resources/views/dashboard/admin/domains/index.blade.php
@@ -122,8 +122,6 @@
                 {{--                        </button>--}}
                 {{--                    </a>--}}
                 {{--                </div>--}}
-            <div class="py-4">
-                <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
             </div>
         </x-main-content>
     </x-layout>
