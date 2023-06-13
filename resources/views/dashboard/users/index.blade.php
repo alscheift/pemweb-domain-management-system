1d75185d09e0da3b8f19ec0d292ff9d0e50b790f
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-13 07:03:02 +0700

    Add main component for admins page (users,units, servers,domains)

---
+++ resources/views/dashboard/users/index.blade.php
@@ -0,0 +1,11 @@
+<x-template>
+    <x-sidebar/>
+    <x-layout>
+        <x-header/>
+        <x-main-content heading="Users">
+            <div class="py-4">
+                <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
+            </div>
+        </x-main-content>
+    </x-layout>
+</x-template>
