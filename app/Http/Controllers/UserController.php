fc3f75e59ffc1c4176c996eab5b569d429d534ee
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-13 11:44:19 +0700

    Admin add user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,5 +12,27 @@ class UserController extends Controller
     public function index(): View
     {
         return view('dashboard.admin.users.index');
+    }
+
+    public function create(): View
+    {
+        return view('dashboard.admin.users.create');
+    }
+
+    public function store(): RedirectResponse
+    {
+        $attributes = request()->validate([
+            'username' => 'required',
+            'password' => 'required',
+
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'unit_id' => ''
+        ]);
+
+        User::create($attributes);
+
+        return redirect(route('users'))->with('success', 'User berhasil ditambahkan');
     }
 }
