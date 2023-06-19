3f0ccac0ea382e116a36d3017f4c4f8a57433c28
Afif Nur Fauzi <58578965+alscheift@users.noreply.github.com>
2023-06-19 14:02:53 +0700

    Install flowbite (#13)
    npm install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
