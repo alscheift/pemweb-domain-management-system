247f62bf87e602f3be9ef8c5bd7999484b2b76da
Afif Nur Fauzi <alscheift@gmail.com>
2023-06-20 14:19:16 +0700

    Input and submit button component

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,11 @@
+@props(['name' => 'any', 'labelName' => null, 'type' => 'text'])
+
+<div>
+    <label class="text-gray-700 dark:text-gray-200" for="{{$name}}">{{$slot}}</label>
+    <input id="{{$name}}" name="{{$name}}" type="{{$type}}"
+           value="{{old($name)??''}}"
+           class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+    @error($name)
+    <p class="text-red-500 text-xs">{{ $message }}</p>
+    @enderror
+</div>
---
+++ resources/views/components/forms/submit.blade.php
@@ -0,0 +1,7 @@
+<div class="flex justify-end mt-6">
+    <button
+        type="submit"
+        class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+        Save
+    </button>
+</div>
