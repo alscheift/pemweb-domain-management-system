ebe5c52481c2e1b4cfe37c65f8ca278740a49e6a
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-06-24 23:13:50 +0700

    add is_done in notifications migration

--- database/migrations/2023_05_15_103037_create_notifications_table.php
+++ database/migrations/2023_05_15_103037_create_notifications_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('status');
             $table->text('description');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
