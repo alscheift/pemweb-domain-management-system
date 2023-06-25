35fcf076562c6b3d10ae0ee1fed5db37402fad5b
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-06-25 19:44:30 +0700

    change faker to id_ID and fix regex phone number

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,14 +18,24 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker::create('id_ID');
+
         return [
-            'email' => fake()->unique()->safeEmail(),
-            'username' => fake()->userName(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
+            'username' => $faker->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'name' => fake()->name(),
-            'phone' => fake()->phoneNumber(),
+            'phone' => $faker->regexify('/^(\+62)\d{10,12}$/'),
             'is_admin' => false,
             'unit_id' => null
+
+            // 'email' => fake()->unique()->safeEmail(),
+            // 'username' => fake()->userName(),
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'name' => fake()->name(),
+            // 'phone' => fake()->phoneNumber(),
+            // 'is_admin' => false,
+            // 'unit_id' => null
         ];
     }
 
