5224f08612ec32fa55c23c84fe6e29a90e25adb7
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-07-02 21:34:52 +0700

    add seeder domains

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Domain;
+use App\Models\DomainImages;
 use App\Models\Server;
 use App\Models\Unit;
 use App\Models\User;
@@ -302,6 +303,294 @@ class DatabaseSeeder extends Seeder
             'core_processor_count' => '2',
             'ram' => '8',
             'unit_id' => $unit8->id
+        ]);
+
+        $domain1 = Domain::create([
+            'name' => 'Main Web Physics',
+            'description' => 'Main Web Physics',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://physics.mipa.uns.ac.id',
+            'user_id' => $user1->id,
+            'server_id' => $server1->id
+        ]);
+
+        $domainImages1 = DomainImages::create([
+            'domain_id' => $domain1->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain2 = Domain::create([
+            'name' => 'Main Web Biology',
+            'description' => 'Main Web Biology',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://biology.mipa.uns.ac.id',
+            'user_id' => $user1->id,
+            'server_id' => $server1->id
+        ]);
+
+        $domainImages2 = DomainImages::create([
+            'domain_id' => $domain2->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain3 = Domain::create([
+            'name' => 'Main Web Himabio Biology',
+            'description' => 'Main Web Himabio Biology',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://himabio.mipa.uns.ac.id/',
+            'user_id' => $user1->id,
+            'server_id' => $server1->id
+        ]);
+
+        $domainImages3 = DomainImages::create([
+            'domain_id' => $domain3->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain4 = Domain::create([
+            'name' => 'Main Web Medical',
+            'description' => 'Main Web Medical',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://prodikedokteran.fk.uns.ac.id/',
+            'user_id' => $user2->id,
+            'server_id' => $server3->id
+        ]);
+
+        $domainImages4 = DomainImages::create([
+            'domain_id' => $domain4->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain5 = Domain::create([
+            'name' => 'Main Web Kebidanan',
+            'description' => 'Main Web Kebidanan',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://sarjanakebidanan.fk.uns.ac.id/',
+            'user_id' => $user2->id,
+            'server_id' => $server3->id
+        ]);
+
+        $domainImages5 = DomainImages::create([
+            'domain_id' => $domain5->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain6 = Domain::create([
+            'name' => 'Main Web Medical Specialization',
+            'description' => 'Main Web Medical Specialization',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://ppds.fk.uns.ac.id/',
+            'user_id' => $user2->id,
+            'server_id' => $server3->id
+        ]);
+
+        $domainImages6 = DomainImages::create([
+            'domain_id' => $domain6->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain7 = Domain::create([
+            'name' => 'Main Web Education Indonesian Language',
+            'description' => 'Main Web Education Indonesian Language',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://bastind.fkip.uns.ac.id/',
+            'user_id' => $user3->id,
+            'server_id' => $server5->id
+        ]);
+
+        $domainImages7 = DomainImages::create([
+            'domain_id' => $domain7->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain8 = Domain::create([
+            'name' => 'Main Web Historical Education',
+            'description' => 'Main Web Historical Education',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://sejarah.fkip.uns.ac.id/',
+            'user_id' => $user3->id,
+            'server_id' => $server5->id
+        ]);
+
+        $domainImages8 = DomainImages::create([
+            'domain_id' => $domain8->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain9 = Domain::create([
+            'name' => 'Main Web Geografi Education',
+            'description' => 'Main Web Geografi Education',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://geografi.fkip.uns.ac.id/',
+            'user_id' => $user3->id,
+            'server_id' => $server5->id
+        ]);
+
+        $domainImages9 = DomainImages::create([
+            'domain_id' => $domain9->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain10 = Domain::create([
+            'name' => 'Main Web Informatics',
+            'description' => 'Main Web Informatics',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://if.fatisda.uns.ac.id/',
+            'user_id' => $user4->id,
+            'server_id' => $server7->id
+        ]);
+
+        $domainImages10 = DomainImages::create([
+            'domain_id' => $domain10->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain11 = Domain::create([
+            'name' => 'Main Web Himaster',
+            'description' => 'Main Web Himaster',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://himaster.fatisda.uns.ac.id/',
+            'user_id' => $user4->id,
+            'server_id' => $server7->id
+        ]);
+
+        $domainImages11 = DomainImages::create([
+            'domain_id' => $domain11->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain12 = Domain::create([
+            'name' => 'Main Web Service FH',
+            'description' => 'Main Web Service FH',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://layanan.hukum.uns.ac.id/',
+            'user_id' => $user5->id,
+            'server_id' => $server9->id
+        ]);
+
+        $domainImages12 = DomainImages::create([
+            'domain_id' => $domain12->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain13 = Domain::create([
+            'name' => 'Main Web Indonesian Literature',
+            'description' => 'Main Web Indonesian Literature',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://bastind.fkip.uns.ac.id/',
+            'user_id' => $user6->id,
+            'server_id' => $server11->id
+        ]);
+
+        $domainImages13 = DomainImages::create([
+            'domain_id' => $domain13->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain14 = Domain::create([
+            'name' => 'Main Web Machine Engineering',
+            'description' => 'Main Web Machine Engineering',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://mesin.ft.uns.ac.id/',
+            'user_id' => $user7->id,
+            'server_id' => $server13->id
+        ]);
+
+        $domainImages14 = DomainImages::create([
+            'domain_id' => $domain14->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain15 = Domain::create([
+            'name' => 'Main Web Architecture Engineering',
+            'description' => 'Main Web Architecture Engineering',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://arsitektur.ft.uns.ac.id/',
+            'user_id' => $user7->id,
+            'server_id' => $server13->id
+        ]);
+
+        $domainImages15 = DomainImages::create([
+            'domain_id' => $domain15->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain16 = Domain::create([
+            'name' => 'Main Web Industrial Engineering',
+            'description' => 'Main Web Industrial Engineering',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://industri.ft.uns.ac.id/',
+            'user_id' => $user7->id,
+            'server_id' => $server13->id
+        ]);
+
+        $domainImages16 = DomainImages::create([
+            'domain_id' => $domain16->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain17 = Domain::create([
+            'name' => 'Main Web International Relations',
+            'description' => 'Main Web International Relations',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://hi.fisip.uns.ac.id/',
+            'user_id' => $user8->id,
+            'server_id' => $server15->id
+        ]);
+
+        $domainImages17 = DomainImages::create([
+            'domain_id' => $domain17->id,
+            'images' => 'domain_images/default-image.png'
+        ]);
+
+        $domain18 = Domain::create([
+            'name' => 'Main Web public administration',
+            'description' => 'Main Web public administration',
+            'application_type' => 'Website',
+            'port' => '80',
+            'http_status' => 'Active',
+            'url' => 'https://adneg.fisip.uns.ac.id/',
+            'user_id' => $user8->id,
+            'server_id' => $server15->id
+        ]);
+
+        $domainImages18 = DomainImages::create([
+            'domain_id' => $domain18->id,
+            'images' => 'domain_images/default-image.png'
         ]);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
