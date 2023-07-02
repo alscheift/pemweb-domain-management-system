ee193d9a65b4bd7fc110858ade99cda7d0aaea33
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-07-02 20:06:46 +0700

    make modal from marks as done button

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -11,75 +11,39 @@
     </button>
     <div class="flex flex-1 justify-between px-4">
         <div class="flex flex-1">
-            {{-- <form class="flex w-full md:ml-0" action="#" method="GET">
-                <label for="search-field" class="sr-only">Search</label>
-                <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                        <!-- Heroicon name: mini/magnifying-glass -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                            fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                                clip-rule="evenodd"/>
-                        </svg>
-                    </div>
-                    @foreach(request()->except('search') as $key => $value)
-                        <input type="hidden" name="{{ $key }}" value="{{ $value }}"/>
-                    @endforeach
-                    <input type="search" class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
-                        name="search" value="{{ request('search') }}" placeholder="Search"/>
-                </div>
-            </form> --}}
         </div>
         <div class="ml-4 flex items-center md:ml-6">
-            <button type="button"
-                    class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="sr-only">View notifications</span>
-                <!-- Heroicon name: outline/bell -->
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
-                </svg>
-            </button>
-
-            <!-- Profile dropdown -->
             <div class="relative ml-3">
                 <div>
                     <button type="button"
                             class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                         <span class="sr-only">Open user menu</span>
-                        <img class="h-8 w-8 rounded-full"
-                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                             alt="">
+                        <img class="h-8 w-8 rounded-full border-2 border-gray-600"
+                            src="https://icon-library.com/images/default-profile-icon/default-profile-icon-6.jpg"
+                            alt="">
                     </button>
                 </div>
-
-                <!--
-                  Dropdown menu, show/hide based on menu state.
-
-                  Entering: "transition ease-out duration-100"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                -->
-                <!-- TODO: Add dropdown functionality -->
-                <div
+                <div id="user-menu"
                     class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                     tabindex="-1">
                     <!-- Active: "bg-gray-100", Not Active: "" -->
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                       id="user-menu-item-0">Your Profile</a>
+                    <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300">
+                        <a href="{{ route('profile') }}" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                    </div>
 
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                       id="user-menu-item-1">Settings</a>
-
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                       id="user-menu-item-2">Sign out</a>
+                    <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300">
+                        <form action="/logout" method="POST">
+                            @csrf
+    
+                            <button type="submit"
+                                role="menuitem" tabindex="-1" id="user-menu-item-2">
+                                Logout
+                            </button>
+                        </form>
+                    </div>
+                    
                 </div>
             </div>
         </div>
