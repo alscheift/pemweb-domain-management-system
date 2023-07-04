8f95ce6b4144f33046a2de0f598c3c27f1072ee1
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-04 19:56:25 +0700

    Added unresolved Issue

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -37,6 +37,12 @@
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Domain</h5>
                 <span class="ml-3 text-lg font-medium text-gray-700 dark:text-gray-400">20</span>
             </a>
+            <a href="#" class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Unresolved Issue</h5>
+                <span class="ml-3 text-lg font-medium text-gray-700 dark:text-gray-400">20</span>
+            </a>
+
+
         </div>
     </div>
 </x-layouts.dashboard>
