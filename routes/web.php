9b1d9eecf72fa91094420fa776abd38a30d79b97
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-11 15:13:23 +0700

    domains/{domain} route misplacement

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,6 @@ Route::middleware('auth')->group(function () {
 
     // Domains
     Route::get('/domains', [DomainController::class, 'index'])->name('domains');
-    Route::get('/domains/{domain}', [DomainController::class, 'show'])->name('domains.show');
 
     Route::middleware('pic')->group(function () {
         Route::get('/domains/create', [DomainController::class, 'create'])->name('domains.create');
@@ -78,6 +77,7 @@ Route::middleware('auth')->group(function () {
         Route::patch('/domains/{domain}', [DomainController::class, 'update'])->name('domains.update');
         Route::delete('/domains/{domain}', [DomainController::class, 'destroy'])->name('domains.destroy');
     });
+    Route::get('/domains/{domain}', [DomainController::class, 'show'])->name('domains.show');
 
     // Route::get('/domains/export-excel', [DomainController::class, 'viewExcel'])->name('domains.export');
 
